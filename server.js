6ddc2d17b157112fbe57b78708c3c6f3e2923a51
AidanaTur7777 <turdaeva.002@gmail.com>
2021-03-10 21:00:45 +0600

    server

---
+++ server.js
@@ -0,0 +1,138 @@
+const express = require('express')
+const shortid = require('shortid')
+const cors = require('cors')
+
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+// Set some defaults (required if your JSON file is empty)
+db.defaults({
+  inventory: [
+    {
+      id: shortid.generate(),
+      name: 'Скакалка',
+      isGoodCondiyion: true,
+      quantity: 10,
+    },
+    {
+      id: shortid.generate(),
+      name: 'Штанга',
+      isGoodCondiyion: true,
+      quantity: 0,
+    },
+    {
+      id: shortid.generate(),
+      name: 'Гантели',
+      isGoodCondiyion: true,
+      quantity: 20,
+    },
+
+    {
+      id: shortid.generate(),
+      name: 'Блины',
+      isGoodCondiyion: true,
+      quantity: 20,
+    },
+  ],
+}).write()
+
+const app = express()
+const port = 3000
+
+app.use(cors())
+
+app.use(express.json())
+
+app.get('/inventory', (req, res) => {
+  let inventory = db.get('inventory')
+  res.send(inventory)
+})
+
+app.get('/inventory/detail/:id', (req, res) => {
+  const { id } = req.params
+
+  const item = db.get('inventory').find({ id })
+
+  if (!item) {
+    res.status(404).send(`ID ${id} is not found!`)
+  }
+
+  res.send(item)
+})
+
+app.post('/inventory/create', (req, res) => {
+  let newInventory = {
+    id: shortid.generate(),
+    ...req.body,
+  }
+
+  db.get('inventory').push(newInventory).write()
+
+  res.send(newInventory)
+})
+
+app.put('/inventory/update/:id', (req, res) => {
+  let { id } = req.params
+
+  let item = db.get('inventory').find({ id }).value()
+
+  let updatedInventory = {
+    ...item,
+    ...req.body,
+  }
+
+  db.get('inventory').find({ id }).assign(updatedInventory).write()
+
+  res.send(updatedInventory)
+})
+
+// GET /treiners/
+// GET /treiners/detail/:id
+
+const treiners = [
+  {
+    id: shortid.generate,
+    name: 'Kate',
+    isGoodCondiyion: true,
+    age: 21,
+  },
+  {
+    id: shortid.generate,
+    name: 'Denis',
+    isGoodCondiyion: true,
+    age: 20,
+  },
+  {
+    id: shortid.generate,
+    name: 'Sabina',
+    isGoodCondiyion: true,
+    age: 19,
+  },
+
+  {
+    id: shortid.generate,
+    name: 'Kairat',
+    isGoodCondiyion: true,
+    age: 18,
+  },
+]
+app.get('/treiners', (req, res) => {
+  res.send(treiners)
+})
+
+app.get('/treiners/deteil/:id', (req, res) => {
+  const { id } = req.params
+  const item = treiners.find((item) => item.id === id)
+
+  if (!item) {
+    res.status(404).send(`ID ${id} is not found!`)
+  }
+
+  res.send(item)
+})
+app.listen(port, () => {
+  console.log(`Server is running now at http://localhost:3000/`)
+})
